00defeee8adf001fcd392a41b23df57103163834
Vinícius Krolow <krolow@gmail.com>
2011-07-28 11:27:34 -0300

    fixing the http server

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
 var sys = require('sys');
 var ws = require('websocket-server');
-
+var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
 
 var server = ws.createServer();
 
 var clients = {};
 var online = 0;
+
+
+http.createServer(function(request, response) {
+    var uri = url.parse(request.url).pathname;
+    var filename = path.join(process.cwd(), 'client/' + uri);
+    path.exists(filename, function(exists) {
+    	if(!exists) {
+    		response.writeHead(404, {"Content-Type": "text/plain"});
+    		response.write("404 Not Found\n");
+    		response.end();
+    		return;
+    	}
+
+    	fs.readFile(filename, "binary", function(err, file) {
+    		if(err) {
+    			response.writeHead(500, {"Content-Type": "text/plain"});
+    			response.write(err + "\n");
+    			response.end();
+    			return;
+    		}
+
+    		response.writeHead(200);
+    		response.write(file, "binary");
+    		response.end();
+    	});
+    });
+}).listen(8090);
+
+console.log('HTTP server running at port 8090');
 
 server.addListener('connection', function (connection) {
 
@@ -33,4 +65,4 @@ server.addListener('connection', function (connection) {
 
 server.listen(3040);
 
-console.log('Server running at port 3040');
+console.log('Websocket running at port 3040');
